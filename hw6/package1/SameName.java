3b4309a50168ecf430d4eb475c396c07c916e0c6
Alexander Ryabokon <leonid555100@gmail.com>
2023-03-11 14:50:16 +0200

    Optional HW4 done

--- hw6/package1/SameName.java
+++
@@ -1,7 +0,0 @@
-package org.hillel.package1;
-
-public class SameName {
-    public SameName() {
-        System.out.println("SameName package1 object created!");
-    }
-}
--- hw6/package2/SameName.java
+++
@@ -1,7 +0,0 @@
-package org.hillel.package2;
-
-public class SameName {
-    public SameName() {
-        System.out.println("SameName package1 object created!");
-    }
-}
