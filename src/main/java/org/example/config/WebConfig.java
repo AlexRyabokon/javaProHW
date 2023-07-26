934b67527696eab033b90a0bab57ac46fa86de75
Alexander Ryabokon <91755763+AlexRyabokon@users.noreply.github.com>
2023-07-26 08:37:06 +0300

    removed context from WebConfig.java

--- src/main/java/org/example/config/WebConfig.java
+++ src/main/java/org/example/config/WebConfig.java
@@ -4,9 +4,7 @@ package org.example.config;
 import org.example.entities.Order;
 import org.example.entities.Product;
 import org.example.repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,15 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan("org.example")
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    private final ApplicationContext context;
-
-
-    public WebConfig(ApplicationContext context) {
-        this.context = context;
-    }
-
-
-
+   
     @Bean
     public OrderRepository orderRepository(){
         return new OrderRepository();
